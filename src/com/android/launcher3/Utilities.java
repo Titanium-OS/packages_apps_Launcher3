26927390a9c7772656fd0243dfa4056d43b3bd40
ElDainosor <eldainosor@gmail.com>
2018-11-01 21:22:10 -0500

    Set a toggle to show Search bar on first screen
    Instead of hardcoding it on the FeatureFlags, let's make it
    toggleable as any other option would do
    
    Change-Id: I59893308efa357d25bdd3d53e31904e90c4d84ca
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -129,6 +129,8 @@ public final class Utilities {
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
 
+    public static final String KEY_SHOW_SEARCHBAR = "pref_show_searchbar";
+
     private static final long WAIT_BEFORE_RESTART = 250;
 
     /**
@@ -712,5 +714,10 @@ public final class Utilities {
     public static boolean isWorkspaceEditAllowed(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_EDIT, true);
+    }
+
+    public static boolean showQSB(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_SHOW_SEARCHBAR, true);
     }
 }
