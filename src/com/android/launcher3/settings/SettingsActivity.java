6ca1ef71ee03d3a491a7280139f70c23c00fe3aa
hundeva <hundeva@gmail.com>
2019-10-12 07:33:14 +0000

    Launcher3: Add restart action for toggling settings
    Useful to update some settings that only change on restarting the launcher.
    
    It's a clean up version of the following commits:
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/10529fedf53e6f79abe380832dd5e7deca329b14
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/a3357bb5f9defef8d91f4f2b663d45e422038447
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/74cba5a81ae39c6694bc4b7ddc8cd64440c57bfa
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/81e08c2033400625c0701a6bcb5cd9fd4504f5fe
    
    Co-authored-by: Alex Cruz <alex@dirtyunicorns.com>
    Co-authored-by: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -33,6 +33,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.LauncherTab;
 import com.android.launcher3.R;
@@ -257,6 +258,9 @@ public class SettingsActivity extends Activity
                 mNotificationDotsObserver.unregister();
                 mNotificationDotsObserver = null;
             }
+            // if we don't press the home button but the back button to close Settings,
+            // then we must force a restart because the home button watcher wouldn't trigger it
+            LauncherAppState.getInstanceNoCreate().checkIfRestartNeeded();
             super.onDestroy();
         }
     }
